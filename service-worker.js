c1586b6ce21552c79a44d0b06941d81d13edcee7
Abuali223 <kamerasony24@gmail.com>
2025-08-14 17:50:12 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('nasiya-cache-v1').then(cache=>cache.addAll([
+  e.waitUntil(caches.open('nasiya-cache-v2').then(cache=>cache.addAll([
     './','./index.html','./manifest.json'
   ])));
 });
